5d655bb8b806a79092ac4b8b934dae09fbeda3bb
Prabhash-109 <prabhashjha92560@gmail.com>
2025-09-29 10:49:05 +0530

    Update Website Name

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className="navbar-container">
         {/* Logo / Brand */}
         <Link to="/" className="navbar-brand">
-          🚗 CarRental
+          🚗 GoDrive
         </Link>
 
         {/* Links */}
